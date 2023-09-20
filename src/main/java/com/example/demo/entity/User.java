c76f96dc924482084d222561b46e4ed297119226
hieunoobpro <hieudsgh@gmail.com>
2023-09-20 20:48:54 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,21 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+}
